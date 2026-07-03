5d70d63007e7e3b9742e2ee27a6c9a7629f3351b
akiracchi2000 <akiracchi2000@gmail.com>
2026-07-03 12:05:54 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'clear-maker-2a-v1.4.58';
+const APP_ASSETS = [
+    './',
+    './index.html',
+    './student.css?v=1.4.58',
+    './student.js?v=1.4.58',
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(names => Promise.all(
+                names
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin !== self.location.origin) return;
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                if (response.ok) {
+                    const copy = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+                }
+                return response;
+            })
+            .catch(() => caches.match(event.request, { ignoreSearch: true }))
+    );
+});
